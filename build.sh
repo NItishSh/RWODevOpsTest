b21a0d67ede2037d297144f1231b3e78a73f6380
NitishSh <csnitish@gmail.com>
2019-02-06 04:39:31 +0000

    adding build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+g++ -std=c++11 -I/usr/include/boost/asio -I/usr/include/boost -o binary.out main.cpp connection.cpp connection_manager.cpp mime_types.cpp reply.cpp request_handler.cpp request_parser.cpp server.cpp -lboost_system -lboost_thread -lpthread
+echo "r is ${1}"
+b=$BUILD_BUILDNUMBER
+echo "b is ${b}"
+c=$(git rev-parse --short HEAD )
+echo "c is ${c}"
+version=$r.$b.$c
+echo "version is ${version}"
+cp binary.out binary$version.out
+echo $version | tee readme.txt
+
