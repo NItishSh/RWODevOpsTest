08d68719ec69586d092df5732c39b6c1d68f3c2c
nitish.h <nitish.h@esoptra.com>
2019-02-06 11:11:00 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -xe 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 g++ -std=c++11 -I/usr/include/boost/asio -I/usr/include/boost -o binary.out main.cpp connection.cpp connection_manager.cpp mime_types.cpp reply.cpp request_handler.cpp request_parser.cpp server.cpp -lboost_system -lboost_thread -lpthread
 echo "r is ${1}"
 b=$BUILD_BUILDNUMBER
