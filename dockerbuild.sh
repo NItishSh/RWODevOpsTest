f3dd8d15bf2a74b137b21177f6369c1e736948bc
NitishSh <csnitish@gmail.com>
2019-02-06 06:52:30 +0000

    updated docker build script

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -xe
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
 version=$(basename binary* | cut -d "." -f2,3,4)
 sudo docker build -t devopstest:latest -t devopstest:$version .
