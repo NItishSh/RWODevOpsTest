a5e3c795db25105b055a022933ca23d209adb275
NitishSh <csnitish@gmail.com>
2019-02-06 08:08:55 +0000

    updated docker stuff

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ RUN apt-get update && \
     apt-get install -y libboost-all-dev
 
 COPY binary*.out /usr/local/bin/binary.out
-CMD ["/usr/local.bin/binary.out"]
+CMD ["/usr/local/bin/binary.out"]
  
 EXPOSE 80 443
--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,5 +4,10 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-version=$(basename binary* | cut -d "." -f2,3,4)
-sudo docker build -t devopstest:latest -t devopstest:$version .
+#version=$(basename binary* | cut -d "." -f2,3,4)
+sudo docker build -t csnitsh/devopstest:latest #devopstest:latest -t devopstest:$version .
+
+#sudo docker run -d -p 80:80 csnitsh/devopstest
+
+#sudo docker push csnitsh/devopstest:latest
+#sudo docker push csnitsh/devopstest:$version
